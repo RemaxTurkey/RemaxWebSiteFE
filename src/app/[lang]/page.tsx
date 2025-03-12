53c211235c8df8fd831a7484452da0d8dff7f753
dcselek <canselek2434@gmail.com>
2025-03-12 11:37:18 +0300

    Update home page layout with fast categories and improved styling

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import HomeFilter from "@/components/HomeFilter/HomeFilter";
+import FastCategories from "@/components/FastCategories/FastCategories";
 
 const page = () => {
   return (
     <div className="container min-h-screen mx-auto">
-      <div className="flex flex-col items-center gap-6 md:bg-[url('/images/home-bg.png')] md:bg-cover md:bg-center md:bg-no-repeat">
+      <div className="md:bg-cover md:bg-center md:bg-no-repeat flex flex-col items-center gap-6 md:gap-14 w-full">
         <span className="text-base md:text-2xl md:text-black font-normal text-white capitalize text-center">
-            Sektörün en çok <br/>
-            <span className="font-semibold">tercih edilen markası</span>
+          Sektörün en çok <br />
+          <span className="font-semibold">tercih edilen markası</span>
         </span>
-        <HomeFilter />
+        <div className="flex flex-col gap-6 md:gap-14 w-full max-w-3xl mx-auto">
+          <HomeFilter />
+          <FastCategories />
+        </div>
       </div>
     </div>
   );
