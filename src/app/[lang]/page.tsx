f6e47f6994f23d6f4b6cdfd5801194993f15d15c
dcselek <canselek2434@gmail.com>
2025-03-13 13:00:40 +0300

    Refactor home page layout by replacing PropertyCardMiniExample with KesfedinSection and integrating FastCategories. Update styles for improved responsiveness and visual consistency.

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import HomeFilter from "@/components/HomeFilter/HomeFilter";
-import FontTest from "@/components/FontTest";  
-import ColorPalette from "@/components/ColorPalette/ColorPalette";
-import PropertyCardMiniExample from "@/components/PropertyCardMini/PropertyCardMiniExample"; 
+import FastCategories from "@/components/FastCategories/FastCategories";
+import KesfedinSection from "@/components/Sections/Home/Kesfedin/KesfedinSection";
 const page = () => {
   return (
-    <div className="container min-h-screen mx-auto">
-      <div className="h-[calc(100vh-50px)] bg-white rounded-3xl mx-0 py-0 mb-0 md:mx-5 mt-2 md:py-6 md:mb-10">
-        <div className="md:bg-cover md:bg-center md:bg-no-repeat flex flex-col items-center gap-6 md:gap-14 mt-66px md:mt-[215px] w-full">
+    <div className="container max-w-screen-2xl min-h-screen mx-auto">
+      <div className="h-[276px] md:h-[calc(100vh-20px)] bg-white rounded-none md:rounded-3xl mx-0 py-0 mb-0 md:mx-5 mt-0 md:mt-2 md:py-6 md:mb-10">
+        <div className="md:bg-cover md:bg-center md:bg-no-repeat flex flex-col items-center gap-6 md:gap-14 w-full justify-center h-full">
           <span className="text-base md:text-2xl md:text-black font-normal text-white capitalize text-center">
             Sektörün en çok <br />
             <span className="font-semibold">tercih edilen markası</span>
           </span>
-          <HomeFilter />
-          <FontTest /> 
-          <ColorPalette/>
+          <div className="flex flex-col gap-6 md:gap-14 w-full max-w-3xl mx-auto">
+            <HomeFilter />
+            <FastCategories />
+          </div>
         </div>
       </div>
-      <div className="w-full">
-        <PropertyCardMiniExample />
+      <div className="w-full mt-10 px-40">
+        <KesfedinSection />
       </div>
     </div>
   );
